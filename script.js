42004a7cac96057ac0f72bd8ad247c7fd0933164
AntonRickert <antonrickert.dev@gmail.com>
2022-09-03 15:43:55 +0200

    Add Convas size slider and Random RGB button

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const grid_container = document.querySelector('#grid-container');
-
+const valueContainer = document.querySelector('#value');
 const startText = document.createElement('div');
 startText.textContent = 'Click "New Grid" button.';
 startText.classList.add('startText');
 grid_container.appendChild(startText);
 
 const newGrid = document.querySelector('#newGrid');
-newGrid.addEventListener('click', makeCanvas);
+newGrid.addEventListener('click', makeCanvasBlue);
+
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', clearCanvas);
+
+const slider = document.querySelector('input[type="range"]');
+const value = document.querySelector('#valueOutput');
+slider.addEventListener('input', updateValue);
+
+const randomRGB = document.querySelector('#random');
+randomRGB.addEventListener('click', makeCanvasRandom);
 
 let div;
 let nodesize;
@@ -16,8 +24,18 @@ let gridCalc;
 let borderRadius;
 let gridNodes;
 let nodeArr;
+function random_rgba() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`;
+    return rgb;
+}
 
-
+function updateValue(){
+    value.textContent = slider.value + ' x ' + slider.value;
+}
 
 function clearCanvas() {
     for (let i = 0; i < nodeArr.length; i++) {
@@ -26,19 +44,25 @@ function clearCanvas() {
 }
 
 
-function makeCanvas() {
+function makeCanvasBlue() {
     calcGrid();
     drawGrid();
-    drawPixel();
+    drawPixelBlue();
+}
+
+function makeCanvasRandom() {
+    calcGrid();
+    drawGrid();
+    drawPixelRandom();
 }
 
 function calcGrid() {
     grid_container.innerHTML = '';
-    let gridSize = prompt('Enter grid size from 1 - 100');
+    let gridSize = slider.value;
         
     if (gridSize > 100 || gridSize < 1) {
         alert('Please enter a valid grid size.')
-        calcGrid();
+        return;
     }
     gridCalc = gridSize * gridSize;
     nodesize = 100 / gridSize + '%';
@@ -58,13 +82,23 @@ for (let i = 0; i < gridCalc; i++) {
     }
 }
 
-function drawPixel() {
+function drawPixelBlue() {
     gridNodes = document.querySelectorAll('.grid-node');
     nodeArr = Array.from(gridNodes);
 
     for (let i = 0; i < nodeArr.length; i++) {
         nodeArr[i].addEventListener('mouseover', function() {
             nodeArr[i].classList.add('colored');
+        });
+    }
+}
+function drawPixelRandom() {
+    gridNodes = document.querySelectorAll('.grid-node');
+    nodeArr = Array.from(gridNodes);
+
+    for (let i = 0; i < nodeArr.length; i++) {
+        nodeArr[i].addEventListener('mouseover', function() {
+            nodeArr[i].style.backgroundColor = random_rgba();
         });
     }
 }
